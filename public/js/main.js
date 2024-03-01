ee746a672f24f10d1d75c788638b5dc3a9612137
AlexDream13 <alexdream13rus@hotmail.com>
2024-03-01 18:51:11 +0300

    Добавлен swiper slider

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,12 @@ burgerBlock.addEventListener("click", function () {
   document.body.classList.toggle('no-scroll');
 });
 
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
